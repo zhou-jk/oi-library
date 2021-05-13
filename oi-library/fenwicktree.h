6eb4b2bf6711d7b1b0fc6caa4fcf81af558e22eb
zhou-jk <zhou_jk@foxmail.com>
2021-05-13 23:01:26 +0800

    fix bugs

--- oi-library/fenwicktree.h
+++ oi-library/fenwicktree.h
@@ -33,31 +33,3 @@ public:
         return getsum(r - 1) - getsum(l - 1);
     }
 };
-#include <cstdio>
-int main() {
-    int n, q;
-    scanf("%d %d", &n, &q);
-
-    fenwick_tree<long long> fw(n);
-
-    for (int i = 0; i < n; i++) {
-        int a;
-        scanf("%d", &a);
-        fw.add(i, a);
-    }
-
-    for (int i = 0; i < q; i++) {
-        int t;
-        scanf("%d", &t);
-
-        if (t == 0) {
-            int p, x;
-            scanf("%d %d", &p, &x);
-            fw.add(p, x);
-        } else {
-            int l, r;
-            scanf("%d %d", &l, &r);
-            printf("%lld\n", fw.sum(l, r));
-        }
-    }
-}
